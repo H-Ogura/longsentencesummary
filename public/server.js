02480277f01c53d72b27a702c3adf249926faaa1
FUJI\ogurah <ogurah@fsi.co.jp>
2018-10-23 16:30:12 +0900

    make it better

--- public/server.js
+++ public/server.js
@@ -1,9 +1,9 @@
 // 送信ボタン
 function googleAnalyzeSentiment(){
-    alert(document.getElementById('textarea-id-01').innerText);
+    alert(document.getElementById('textarea-id-01'));
 
     socket.emit('googleAnalyzeSentiment', {
-        content: document.getElementById('textarea-id-01').innerText
+        content: document.getElementById('textarea-id-01')
     }, function(body) {
         console.log(JSON.stringify(JSON.parse(body), undefined, 4));
         document.getElementById('results').innerHTML = JSON.stringify(JSON.parse(body), undefined, 4);
