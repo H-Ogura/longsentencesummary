676af7f4b30eb282641977a8374f6dec1684ad73
FUJI\ogurah <ogurah@fsi.co.jp>
2018-10-23 11:28:43 +0900

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
+'use strict';
+/*
 var http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, World\n');
+  //response.end('Hello, World\n');
 }).listen(process.env.PORT, process.env.IP);
+*/
+const language = require('@google-cloud/language');
+
+// Instantiates a client
+const client = new language.LanguageServiceClient();
+
+// The text to analyze
+const text = 'Hello, world!';
+
+const document = {
+  content: text,
+  type: 'PLAIN_TEXT',
+};
+
+// Detects the sentiment of the text
+client
+  .analyzeSentiment({document: document})
+  .then(results => {
+    const sentiment = results[0].documentSentiment;
+
+    console.log(`Text: ${text}`);
+    console.log(`Sentiment score: ${sentiment.score}`);
+    console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
