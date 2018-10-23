6dbb361ac382f761486c6d2f6ebcca6c38f7e95e
FUJI\ogurah <ogurah@fsi.co.jp>
2018-10-23 16:54:32 +0900

    make it better

--- public/server.js
+++ public/server.js
@@ -1,14 +1,11 @@
 // 送信ボタン
 function googleAnalyzeSentiment(){
-    //alert(document.getElementById('textarea-id-01'));
     var txtObj = document.getElementById('textarea-id-01');
-    alert(txtObj.value);
 
     socket.emit('googleAnalyzeSentiment', {
-        //content: document.getElementById('textarea-id-01')
         content: txtObj.value
     }, function(body) {
-        document.getElementById('results').innerHTML = JSON.stringify(body);
+        document.getElementById('results').innerHTML = JSON.stringify(JSON.parse(body));
     });
 }
 
